c70302d976860aa2be100568155310e39bdfdcd2
makawizy <37740978+makawizy@users.noreply.github.com>
2018-08-30 11:09:15 +0100

    Update finddiff.js

--- js/finddiff.js
+++ js/finddiff.js
@@ -1123,8 +1123,9 @@ var finddiff = {
 			case "pausequit":
 				platspec.play('back');
 				//platspec.dialogconfirm('Leave game', 'Leave this game, are you sure?', 'Yes', 'No', finddiff.quitfromgame);
-				alert("pause");
-				finddiff.dialogconfirm('Leave game', 'Leave this game, are you sure?', 'Yes', 'No', finddiff.quitfromgame(1));
+				
+				//finddiff.dialogconfirm('Leave game', 'Leave this game, are you sure?', 'Yes', 'No', finddiff.quitfromgame(1));
+				finddiff.quitfromgame(1);
 				break;
 			case "rateback":
 				platspec.play('input');
